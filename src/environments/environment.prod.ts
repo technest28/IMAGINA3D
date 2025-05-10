b26787c379b0f9a89318c8f09c1c0731a890de12
Guillermo Galindo <guillegalindo45@gmail.com>
2025-05-10 16:49:03 -0600

    Agrega archivos de configuración de entorno para producción y desarrollo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api-imagina.onrender.com/api/', // URL de la API para otros servicios
+  predictApiUrl: 'https://api-prediction-adoo.onrender.com/' // URL de la API para predicciones
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api/', // URL de la API para otros servicios
+  predictApiUrl: 'http://127.0.0.1:5000/' // URL de la API para predicciones
+};

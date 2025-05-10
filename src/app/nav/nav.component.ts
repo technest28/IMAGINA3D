aa340e2bc0943723d1a9463cc402cf7ca0b211e8
Guillermo Galindo <guillegalindo45@gmail.com>
2025-05-10 16:26:18 -0600

    Agrega barra de navegación con estilos y funcionalidad de menú hamburguesa

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [ NgClass ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
 
+  navbarOpened: boolean = false; // Indica si el menú está abierto
+  scrolled: boolean = false; // Indica si el usuario hizo scroll
+
+  constructor() { }
+
+  // Alternar el estado del menú hamburguesa
+  toggleNavbar() {
+    this.navbarOpened = !this.navbarOpened;
+  }
+
+  // Detectar el scroll y cambiar el estado
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.scrolled = window.pageYOffset > 0;
+  }
 }

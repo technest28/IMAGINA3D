1fa54e78824bcc3d12a5ea8660d67b3cd74ad9c1
Guillermo Galindo <guillegalindo45@gmail.com>
2025-06-12 18:30:26 -0600

    Restablece la configuración del entorno a producción con las URLs de API actualizadas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
-// export const environment = {
-//   production: false,
-//   apiUrl: 'http://localhost:3000/api/', // URL de la API para otros servicios
-//   predictApiUrl: 'http://127.0.0.1:5000/' // URL de la API para predicciones
-// };
+export const environment = {
+  production: true,
+  apiUrl: 'https://api-imagina.onrender.com/api/',
+  predictApiUrl: 'https://api-prediction-adoo.onrender.com/'
+};
